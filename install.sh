f64d1303bd628dbbfc3ae270b994fde7efa32a1c
Richard Ma <richard.ma.19850509@gmail.com>
2012-12-04 10:21:09 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Warning: This install bash script is only for richard_ma
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+cp tomato.sh ~/bin
+cp showtime.sh ~/bin
+cp -R tomato_resources ~/bin
